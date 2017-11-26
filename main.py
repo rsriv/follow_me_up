d168a458bf1889d7298904d8b48ceb8c2b4beb59
Rishi Srivastava <rishi.srivastava98@gmail.com>
2017-11-26 12:55:08 -0500

    added global screen_name; fixed follow_followers logic

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import tweepy
 
 class BoostFollowers:
-    def __init__(self,consumer_key,consumer_secret,access_token_key,access_token_secret):
+    def __init__(self,screen_name,consumer_key,consumer_secret,access_token_key,access_token_secret):
         self.consumer_key = consumer_key
         self.consumer_secret = consumer_secret
         self.access_token_key = access_token_key
         self.access_token_secret = access_token_secret
+        self.screen_name = screen_name
 
         self.auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
         self.auth.set_access_token(access_token_key, access_token_secret)
         self.api = tweepy.API(self.auth)
-
 
     # follow followers based on a given handle
     def follow_followers(self,handle):
         try:
             followers = self.api.followers(handle)
             for follower in followers:
-                self.api.create_friendship(handle)
+                if follower.screen_name != self.screen_name:
+                    self.api.create_friendship(follower.screen_name)
+                    print follower.screen_name
         except tweepy.TweepError as e:
             print "Could not follow followers of " + handle + " at this time. Error: "
             print e
