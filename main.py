a131277c89ae0105ad2b2c5ec1f217b79f75041a
Rishi Srivastava <rishi.srivastava98@gmail.com>
2017-11-26 23:36:37 -0500

    Added randomization to emulate natural user interaction, follower/following ratio check, iterating through all followers

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import tweepy
+import time
+from random import randint
+
 
 class BoostFollowers:
     def __init__(self,screen_name,consumer_key,consumer_secret,access_token_key,access_token_secret):
@@ -6,20 +9,29 @@ class BoostFollowers:
         self.consumer_secret = consumer_secret
         self.access_token_key = access_token_key
         self.access_token_secret = access_token_secret
-        self.screen_name = screen_name
 
         self.auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
         self.auth.set_access_token(access_token_key, access_token_secret)
-        self.api = tweepy.API(self.auth)
+        self.api = tweepy.API(self.auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True,compression=True)
 
     # follow followers based on a given handle
     def follow_followers(self,handle):
         try:
-            followers = self.api.followers(handle)
-            for follower in followers:
-                if follower.screen_name != self.screen_name:
-                    self.api.create_friendship(follower.screen_name)
-                    print follower.screen_name
+            myid = self.api.me().id
+            cursor = tweepy.Cursor(self.api.followers,screen_name=handle)
+            for follower in cursor.items():
+                wait_time = randint(1,30)
+                if follower.id != myid and 1.1*follower.friends_count >= follower.followers_count:
+                    self.api.create_friendship(follower.id)
+                    print 'Followed ' + str(follower.screen_name)
+                    print follower.followers_count
+                    print follower.friends_count
+                    print 'Waiting ' + str(wait_time) + ' secs'
+                    time.sleep(wait_time)
+                else:
+                    print 'Skipped following ' + follower.screen_name
         except tweepy.TweepError as e:
             print "Could not follow followers of " + handle + " at this time. Error: "
             print e
+            time.sleep(60*15)
+            quit()
