56486ba6d693f14526cc9d58a1e0585808c5bfba
Rishi Srivastava <rishi.srivastava98@gmail.com>
2017-12-16 23:19:48 -0500

    created function to unfollow users to maintain followers/following ratio

--- main.py
+++ main.py
@@ -14,18 +14,23 @@ class BoostFollowers:
         self.auth.set_access_token(access_token_key, access_token_secret)
         self.api = tweepy.API(self.auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True,compression=True)
 
+
     # follow followers based on a given handle
     def follow_followers(self,handle):
         try:
             myid = self.api.me().id
             cursor = tweepy.Cursor(self.api.followers,screen_name=handle)
+
+            # iterate through followers of user
             for follower in cursor.items():
-                wait_time = randint(1,30)
+                wait_time = randint(1,38)
+
+                # follow user if following greater than or equal to num followers
                 if follower.id != myid and 1.1*follower.friends_count >= follower.followers_count:
                     self.api.create_friendship(follower.id)
                     print 'Followed ' + str(follower.screen_name)
-                    print follower.followers_count
-                    print follower.friends_count
+                    print 'Followers: ' + str(follower.followers_count)
+                    print 'Friends: ' + str(follower.friends_count)
                     print 'Waiting ' + str(wait_time) + ' secs'
                     time.sleep(wait_time)
                 else:
@@ -33,5 +38,38 @@ class BoostFollowers:
         except tweepy.TweepError as e:
             print "Could not follow followers of " + handle + " at this time. Error: "
             print e
-            time.sleep(60*15)
+            quit()
+
+
+    # unfollow all followers
+    def unfollow_all(self):
+        try:
+            myid = self.api.me().id
+            cursor = tweepy.Cursor(self.api.friends)
+            periodic_delay = randint(289,580)
+            max_count = randint(258,1090)
+            count = 0
+
+            # iterate through users following
+            for following in cursor.items():
+                wait_time = randint(1,31)
+
+                # periodically introduce long delay to emulate human activity
+                if (count >= max_count):
+                    print 'Starting long delay'
+                    wait_time = periodic_delay
+                    periodic_delay = randint(289,580)
+                    max_count = randint(258,1090)
+                    count = 0
+
+                # unfollow user following
+                self.api.destroy_friendship(following.screen_name)
+                print 'Unfollowed ' + str(following.screen_name)
+                print 'Waiting ' + str(wait_time) + ' secs'
+
+                time.sleep(wait_time)
+                count += 1
+
+        except tweepy.TweepError as e:
+            print e
             quit()
